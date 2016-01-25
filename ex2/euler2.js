67de5f6ac451d82fb858a05835bb5734494dd0d7
David Malary <davidmalary@gmail.com>
2016-01-25 11:11:30 -0500

    euler 2 complete

--- ex2/euler2.js
+++ ex2/euler2.js
@@ -2,21 +2,20 @@ console.log('Open Sesame')
 
 var int1 = 1;
 var int2 = 2;
-var int3;
-var count = 2;
+var int3 = 0;
 var intEleven = 0;
 
-while (count < 22) {
+while (int3 <= 4000000) {
   int3 = int1 + int2;
   int1 = int2;
   int2 = int3;
 
-  count++;
-  console.log(int3);
-  if (count % 11 == 0) {
+  // console.log(int3);
+  if (int3 % 2 == 0) {
     intEleven += int3;
   };
 }
 
 console.log(int3);
 console.log(intEleven);
+// answer is intEleven + 2(aka int2)
