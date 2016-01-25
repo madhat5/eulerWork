079e4c20e3c6248779630d6e7a59376ef2dd8e89
David Malary <davidmalary@gmail.com>
2016-01-25 11:01:51 -0500

    good script, testing 4 million

--- ex2/euler2.js
+++ ex2/euler2.js
@@ -1,1 +1,22 @@
 console.log('Open Sesame')
+
+var int1 = 1;
+var int2 = 2;
+var int3;
+var count = 2;
+var intEleven = 0;
+
+while (count < 22) {
+  int3 = int1 + int2;
+  int1 = int2;
+  int2 = int3;
+
+  count++;
+  console.log(int3);
+  if (count % 11 == 0) {
+    intEleven += int3;
+  };
+}
+
+console.log(int3);
+console.log(intEleven);
