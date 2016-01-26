388462c27410932ee8bff592e332ffc3abad8c65
David Malary <davidmalary@gmail.com>
2016-01-26 10:44:22 -0500

    euler3 solved

--- ex3/euler3.js
+++ ex3/euler3.js
@@ -1,1 +1,28 @@
 console.log('open sesame');
+
+var isPrime = function(num){
+  if (num < 2) return(false);
+  if (num === 2) return(true);
+  for (var x = 2; x < num; x++){
+    if (num % x == 0) {
+      return false;
+    }
+  };
+  return true;
+};
+
+var primeAnswer;
+
+function bigPrime(){
+  for (var x = 0; x <= 600851475143; x++){
+    if (600851475143 % x == 0){
+      if (isPrime(x)) {
+        console.log(x);
+        primeAnswer = x;
+      }
+    };
+  };
+  // return primeAnswer;
+};
+
+// console.log(primeAnswer);
