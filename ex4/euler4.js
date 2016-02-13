f0926bc81ee292c6e51d9a5b415925d7ce5fc18b
David Malary <davidmalary@gmail.com>
2016-02-13 14:12:09 -0500

    pre restart commit

---
+++ ex4/euler4.js
@@ -0,0 +1,23 @@
+console.log('Open sesame');
+
+function palindromicNum(){
+  // take num and split into array
+  // for array.length
+  // split array into 2
+  // if array[0 to half]  == array[half to n]
+  //   return true
+}
+
+var num;
+var num2;
+
+function largePal(){
+  for (var x = 100; x <= 999; x++){
+    for (vary y = 100; y <= 999; y++){
+      num = x * y;
+      palindromicNum(num);
+    }
+    num2 = num;
+
+  }
+};
