1b293d8f6c69f70fb46a1257aeb85b3fe90f622f
David Malary <davidmalary@gmail.com>
2018-02-02 15:42:12 -0500

    safety commit

---
+++ ex5/euler5.js
@@ -0,0 +1,19 @@
+console.log('Sim sim salabim');
+
+// divide x by y and check for remainder = 0
+var numAr = [0],
+    finalNum = 0,
+    checkNum = 0
+
+var divNum = function(y) {
+    checkNum = finalNum / y;
+    return checkNum;
+};
+
+for (var x = 0; x <= 20; x++) {
+    var y = 1;
+
+    if (y % x == 0) {
+
+    }
+}
